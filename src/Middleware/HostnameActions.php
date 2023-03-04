7edbdf7f4d1c14df9c0159caa4f4022c112dace5
Qisthi Ramadhani <rama@qisthi.dev>
2023-03-04 10:55:11 +0700

    fix: circle ci (#4)
    * fix: lang folder not found
    
    * fix: MaintenanceModeException deprecation
    
    * fix: validation message hostname invalid

--- src/Middleware/HostnameActions.php
+++ src/Middleware/HostnameActions.php
@@ -22,11 +22,10 @@ use Hyn\Tenancy\Events\Hostnames\Redirected;
 use Hyn\Tenancy\Events\Hostnames\Secured;
 use Hyn\Tenancy\Events\Hostnames\UnderMaintenance;
 use Hyn\Tenancy\Traits\DispatchesEvents;
-use Illuminate\Foundation\Http\Exceptions\MaintenanceModeException;
+use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Redirector;
-use Illuminate\Support\Facades\URL;
 
 class HostnameActions
 {
@@ -104,7 +103,7 @@ class HostnameActions
     protected function maintenance(Hostname $hostname)
     {
         $this->emitEvent(new UnderMaintenance($hostname));
-        throw new MaintenanceModeException($hostname->under_maintenance_since->timestamp, null, "");
+        throw new HttpResponseException(abort(503, 'Under maintenance since: '.$hostname->under_maintenance_since->timestamp));
     }
 
     /**
